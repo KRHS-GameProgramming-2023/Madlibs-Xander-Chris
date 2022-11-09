51459c6e5cde6ef324b9d76a123492263cd10905
alexander <alexander.upton@kearsarge.org>
2022-11-09 10:07:00 -0500

    added more words

--- Getters.py
+++ Getters.py
@@ -115,10 +115,186 @@ def getMeat(prompt, debug = False):
         
     return word  
     
+def getGasStationName(prompt, debug = False):
+    if debug: print("getgetGasStationName Function")
     
+    goodInput = False
     
+    gasStations = ["irving",
+              "chevron corperation",
+              "mobil",
+              "sunoco",
+              "amoco ",
+              "texaco",
+              "Quik Trip",
+              "speedway",
+              "conoco",
+              "sheetz" ,
+              "citgo",
+              "circle K",
+              "murphy USA",
+              "valero",
+              "exxon",
+              "road ranger",
+              "kum and go",
+              "cumberland farms",
+              "buc-ee's",
+              "quickchek",
+              "raceTrac",
+              "royal farms",
+              "wawa",
+              "exxon gas station"]
+
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in gasStations:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that gas station.")
+        
+    return word      
     
+def getVehicle(prompt, debug = False):
+    if debug: print("getgetVehicle Function")
     
+    goodInput = False
+    
+    vehicle = ["car",
+              "truck",
+              "buss",
+              "plane",
+              "boat"]
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in vehicle:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that mode of transportation.")
+        
+    return word  
+
+def getdscrp(prompt, debug = False):
+    if debug: print("getgetdsrp Function")
+    
+    goodInput = False
+    
+    descrip = ["fine",
+              "good",
+              "great",
+              "amazing",
+              "terible",
+              "ok",
+              "bad",
+              "horible",
+              "depresing",
+              "alright",
+              "boat"]
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in descrip:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that description word.")
+        
+    return word  
+
+def getkitchenAplience(prompt, debug = False):
+    if debug: print("getgetkitchenAplience Function")
+    
+    goodInput = False
+    
+    kitchenApliences = ["toaster",
+              "blender",
+              "microwave",
+              "toaster oven",
+              "coffee maker"]
+              
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in kitchenApliences:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that kitchen Aplience.")
+        
+    return word  
+
+def getfurniture(prompt, debug = False):
+    if debug: print("getgetfurniture Function")
+    
+    goodInput = False
+    
+    furniture = ["chair",
+              "table",
+              "coffee table",
+              "sopha",
+              "stool",
+              "lader"]
+              
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in furniture:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that peice of furniture.")
+        
+    return word  
+    
+def getstoreName(prompt, debug = False):
+    if debug: print("getgetstoreName Function")
+    
+    goodInput = False
+    
+    stores = ["target",
+              "marketbasket",
+              "hannafords",
+              "walmart",
+              "game stop"]
+              
+    
+    while not goodInput:
+        word = input(prompt)
+        goodInput = True
+        if isSwear (word):
+            goodInput= False
+            print("\n")
+            print ("don't use language like that")
+        elif word.lower() not in stores:
+            goodInput= False
+            print("\n")
+            print ("sorry, I don't know that store.")
+        
+    return word      
+
+
 def getEmotion(prompt, debug = False):
     if debug: print("getgetEmotion Function")
     
